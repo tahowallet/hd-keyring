83bdbfa413643288fa1dd9d6c0c44c6682f046fb
Matt Luongo <mhluongo@gmail.com>
2021-09-20 14:09:36 -0400

    Verify that addresses are derived compatibly...
    with legacy wallets.

--- test/keyring.test.ts
+++ test/keyring.test.ts
@@ -9,6 +9,41 @@ const validMnemonics = [
   "mad such absent minor vapor edge tornado wrestle convince shy battle region adapt order finish foot follow monitor",
 ]
 
+const validDerivations = [
+  {
+    mnemonic:
+      "square time hurdle gospel crash uncle flash tomorrow city space shine sad fence ski harsh salt need edit name fold corn chuckle resource else",
+    addresses: [
+      "0xca19be978a1d2456d16bde3efb0a5b8946f4a1ce",
+      "0xce73b34e2cdf4e00054c509cc5fdf3882d4a87c8",
+      "0x0b5446d680c0e665ee63508237337c8a9fe31361",
+      "0x342097b215dacc397b7adc11eb54257f6bcb680e",
+      "0x53e5caff572f5d16ae00054a77a252a636e56700",
+      "0x17e02708eeaa9fc8c6ed86b08af1ea2e81cf18f9",
+      "0x4a8d4ad7206c24a1c7e694760dbd35df33068401",
+      "0x2d43d1f8f96ff679511209280617a146b049a999",
+      "0xf260e5482cc567f04f42f6229b694f3a38721ed9",
+      "0xcd29ee2e1fb20fa948451fb66316da280251c439",
+    ],
+  },
+  {
+    mnemonic:
+      "brain surround have swap horror body response double fire dumb bring hazard",
+    addresses: [
+      "0x7b4322b9abe447ce86faa6121b35c84ec36945ad",
+      "0x33a77a26b8523bf21bfd63f81c77f495627304e3",
+      "0x2614fdc904520631f0a24ac3360393e48359fe78",
+      "0xd317dcc257bedf8868b8b41a3f053604e08d3618",
+      "0x0b87d62bec983a9d7832f560377e8a0876fba9cc",
+      "0x6208e7af335ea9422e703b1e688b0e7f17a44a87",
+      "0x74502255857e5fc38945cd6391818726fd9117e5",
+      "0xc3c542dd8057f1c4a92e0bf6aa0248ed37825472",
+      "0xa20ac021efb093f7f56d1e2cff31cca1c6ecac02",
+      "0x260268b1cb9f4b9f6269d6051300057e3a8e1cb5",
+    ],
+  },
+]
+
 const twelveOrMoreWordMnemonics = validMnemonics.filter(
   (m) => m.split(" ").length >= 12,
 )
@@ -98,6 +133,14 @@ describe("HDKeyring", () => {
       expect(await keyring1.getAccounts()).toStrictEqual(
         await keyring2.getAccounts(),
       )
+    })
+  })
+  it("derives the same addresses as legacy wallets", async () => {
+    validDerivations.forEach(async ({ mnemonic, addresses }) => {
+      const keyring = new HDKeyring({ mnemonic })
+      await keyring.addAccountsSync(10)
+      const newAddresses = keyring.getAccountsSync()
+      expect(newAddresses).toStrictEqual(addresses)
     })
   })
   it("signs messages recoverably", async () => {
