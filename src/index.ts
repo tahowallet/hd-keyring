5d76d5d92b6c299d4c4cf93cf1aed0b69ba46e5c
Matt Luongo <mhluongo@gmail.com>
2022-03-06 22:26:03 -0500

    Avoid BIP-32 derivation overflow in addAddressesSync

--- src/index.ts
+++ src/index.ts
@@ -173,6 +173,9 @@ export default class HDKeyring implements Keyring<SerializedHDKeyring> {
   }
 
   addAddressesSync(numNewAccounts = 1): string[] {
+    if (numNewAccounts < 0 || numNewAccounts > 2 ** 31 - 1) {
+      throw new Error("New account index out of range")
+    }
     const numAddresses = this.#addressIndex
 
     for (let i = 0; i < numNewAccounts; i += 1) {
